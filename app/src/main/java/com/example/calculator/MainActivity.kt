d78a53490ec5cda14329c8561ce36ccbb68fd391
Hacktin <isues41@gmail.com>
2020-07-20 16:57:43 +0300

    Instance's define has been edited

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,21 +6,25 @@ import com.example.calculator.business.abs.ICalculatorService
 import com.example.calculator.business.concerete.CalculatorManager
 import com.example.calculator.business.concerete.IntCalculatorManager
 import com.example.calculator.controllers.IntOperationController
+import com.example.calculator.controllers.OperationController
+import com.example.calculator.entities.concerete.IntegerEntity
 import com.example.calculator.operations.abs.ICalculator
+import com.example.calculator.operations.abs.basic.IBasicCalculator
 import com.example.calculator.operations.concerete.IntBaseBasicCalculator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val calculator=IntBaseBasicCalculator()
-    val business=IntCalculatorManager(calculator = calculator)
+    private val calculator:IBasicCalculator<IntegerEntity> =IntBaseBasicCalculator()
+    private val business:CalculatorManager<IntegerEntity,Int> =IntCalculatorManager(calculator)
 
-    val intController=IntOperationController(business)
+    private val intController:OperationController<IntegerEntity,Int> =IntOperationController(business)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
 
 
