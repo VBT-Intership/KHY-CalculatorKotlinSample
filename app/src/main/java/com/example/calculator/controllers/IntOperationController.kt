510cf7134e53839fe400afe2ec954812639c495c
Hacktin <isues41@gmail.com>
2020-07-20 16:59:18 +0300

    abstract Operation controller has been created

--- app/src/main/java/com/example/calculator/controllers/IntOperationController.kt
+++ app/src/main/java/com/example/calculator/controllers/IntOperationController.kt
@@ -1,19 +1,32 @@
 package com.example.calculator.controllers
 
 import com.example.calculator.business.concerete.CalculatorManager
+import com.example.calculator.entities.abstract.INumberEntity
 import com.example.calculator.entities.concerete.IntegerEntity
 import com.example.calculator.entities.enums.Operations
 
 
-class IntOperationController(calculatorManager: CalculatorManager<IntegerEntity,Int>){
-    val _calculatorManager=calculatorManager
 
 
-    fun add(first:IntegerEntity,second:IntegerEntity,options:Operations):Int{
-        return _calculatorManager.add(first,second,options)
+
+abstract  class OperationController<T:INumberEntity,N:Number>(calculatorManager: CalculatorManager<T,N>)
+{
+    abstract fun add (first:T,second:T,operations: Operations):N
+
+    abstract fun extract(first: T,second: T,operations: Operations):N
+}
+
+class IntOperationController(calculatorManager: CalculatorManager<IntegerEntity,Int>) : OperationController<IntegerEntity, Int>(
+    calculatorManager
+) {
+    private val _calculatorManager=calculatorManager
+
+    override fun add(first: IntegerEntity, second: IntegerEntity,operations: Operations): Int {
+        return _calculatorManager.add(first,second,operations)
     }
 
-    fun extract(first:IntegerEntity,second:IntegerEntity,options:Operations):Int{
-        return _calculatorManager.extract(first,second,options)
+    override fun extract(first: IntegerEntity, second: IntegerEntity,operations: Operations): Int {
+        return _calculatorManager.extract(first,second,operations)
     }
+
 }
