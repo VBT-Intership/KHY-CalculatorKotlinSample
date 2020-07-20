219cdcd2bddc8cca874e3c96ecbcf604fd9a2518
Hacktin <isues41@gmail.com>
2020-07-20 16:57:27 +0300

    ICalculator property has been added from ICalculatorService

--- app/src/main/java/com/example/calculator/business/abs/ICalculatorService.kt
+++ app/src/main/java/com/example/calculator/business/abs/ICalculatorService.kt
@@ -2,7 +2,8 @@ package com.example.calculator.business.abs
 
 import com.example.calculator.entities.abstract.INumberEntity
 import com.example.calculator.entities.enums.Operations
+import com.example.calculator.operations.abs.ICalculator
 
 interface ICalculatorService<T:INumberEntity,N:Number>{
-
+   val calculator:ICalculator
 }
